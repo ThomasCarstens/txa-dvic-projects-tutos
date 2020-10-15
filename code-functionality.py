fc76c8e082c6494c206b8a3407bdda5b33195d7a
ThomasCarstens <thomaxarstens@gmail.com>
2020-10-15 11:08:30 +0200

    Yop

--- code-functionality.py
+++ code-functionality.py
@@ -1,6 +1,19 @@
 
 #Welcome to the code hub for the project.
 
+# GAZEBO WORLD
+# Using robot_base
+### See chapter_5_ws workspace.
+
+#GATE RECOGNITION
+## Using a webcam
+### See webcam_door.py script.
+
+
+
+
+
+
 
 
 
@@ -8,3 +21,16 @@
 
 
 #LIDAR
+## hints: use the LaserScan library
+from __future__ import print_function
+from __future__ import division
+import ros
+import rospy
+import math
+from sensor_msgs.msg import LaserScan
+import numpy as np
+from decimal import Decimal
+## hints: 
+
+def find_max(msg):
+	pass
